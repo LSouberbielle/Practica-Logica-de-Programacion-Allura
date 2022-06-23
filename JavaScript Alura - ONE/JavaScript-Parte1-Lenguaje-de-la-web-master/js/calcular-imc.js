ef059c2548b6046d91bca437d43b860d5b39543b
LSouberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-06-23 00:18:03 -0300

    Fixed form not adding new pacients

--- JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/calcular-imc.js
+++ JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/calcular-imc.js
@@ -2,41 +2,61 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Buena Vida Nutrición";
 
-var pacientes = document.querySelectorAll('.paciente');
-console.log(pacientes);
+var pacientes = document.querySelectorAll(".paciente");
 
-    for(var i = 0;i< pacientes.length;i++){
 
-        var paciente = pacientes[i];
+for(var i = 0;i< pacientes.length;i++){
+    var paciente = pacientes[i];
 
-        var tdPeso = paciente.querySelector('.info-peso');
-        var tdAltura = paciente.querySelector('.info-altura');
-        var tdImc = paciente.querySelector('.info-imc');
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-        var peso = tdPeso.textContent;
-        var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-        var pesoEsValido = true;
-        var alturaEsValida = true;  
+    var tdIMC = paciente.querySelector(".info-imc");
 
-        if(peso <=0 || peso >= 1000) {
-            pesoEsValido= false;
-            tdImc.textContent = 'peso incorrecto';
-            paciente.classList.add("paciente-incorrecto");
-        }
-        
-        if(altura <= 0 || altura >= 3.00) {
-        alturaEsValida= false;
-        tdImc.textContent = 'altura incorrecta';
+
+    pesoEsValido = validarPeso(peso);
+    alturaEsValida = validarAltura(altura);
+
+    if(!pesoEsValido){
+        console.log("Peso incorrecto");
+        tdIMC.textContent = "Peso incorrecto";
+        pesoEsValido = false;
         paciente.classList.add("paciente-incorrecto");
-        }
-        
-        if(pesoEsValido && alturaEsValida) {
-            tdImc.textContent = calcularIMC(peso, altura); 
-        }
     }
 
+    if(!alturaEsValida){
+        console.log("Altura incorrecta");
+        tdIMC.textContent = "Altura incorrecta";
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if(pesoEsValido && alturaEsValida){
+        tdIMC.textContent = calcularIMC(peso,altura);
+    }
+
+}
+
 function calcularIMC(peso,altura){
-    var imc  = peso / ( altura * altura);
+    var imc = peso / (altura * altura);
     return imc.toFixed(2);
+}
+
+function validarPeso(peso){
+    if (peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validarAltura(altura){
+    if (altura >= 0 && altura < 3.00){
+        return true;
+    }else{
+        return false;
+    }
 }
--- JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/form.js
+++ JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/form.js
@@ -1,48 +1,52 @@
 var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-botonAdicionar.addEventListener("click", function(event){
-    
+botonAdicionar.addEventListener("click",function(event){
     event.preventDefault();
-    var form = document.querySelector("#form-adicionar");
+
+    var form = document.querySelector("#form-adicionar"); 
     var paciente = capturarDatosPaciente(form);
     var pacienteTr = construirTr(paciente);
+    
+    var errores = validarPaciente(paciente);
+
+    if(errores.length > 0){
+        exhibirMensajesErrores(errores);
+        return;
+    }
+
     var tabla = document.querySelector("#tabla-pacientes");
     tabla.appendChild(pacienteTr);
     form.reset();
+
+    var mensajesErrores = document.querySelector("#mensajes-errores");
+    mensajesErrores.innerHTML = "";
+
 });
 
 function capturarDatosPaciente(form){
-    
+    //capturando los datos del formulario
     var paciente = {
-        
         nombre: form.nombre.value,
         peso: form.peso.value,
         altura: form.altura.value,
-        grasa: form.grasa.value,
+        gordura: form.gordura.value,
         imc: calcularIMC(form.peso.value,form.altura.value)
     }
-    
-    return paciente;
+    return paciente; 
 }
 
 function construirTr(paciente){
-    
-    var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
+     
+       var pacienteTr = document.createElement("tr");     
+       pacienteTr.classList.add("paciente");
+          
+       pacienteTr.appendChild(construirTd(paciente.nombre,"info-nombre"));
+       pacienteTr.appendChild(construirTd(paciente.peso,"info-peso"));
+       pacienteTr.appendChild(construirTd(paciente.altura,"info-altura"));
+       pacienteTr.appendChild(construirTd(paciente.gordura,"info-gordura"));
+       pacienteTr.appendChild(construirTd(paciente.imc,"info-imc"));
 
-    var nombreTd = construirTd(paciente.nombre,"info-nombre");
-    var pesoTd = construirTd(paciente.peso,"info-peso");
-    var alturaTd = construirTd(paciente.altura,"info-altura");
-    var grasaTd = construirTd(paciente.grasa,"info-grasa");
-    var imcTd = construirTd(paciente.imc,"info-imc");
-   
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(grasaTd);
-    pacienteTr.appendChild(imcTd);
-
-    return pacienteTr;
+       return pacienteTr; 
 }
 
 function construirTd(dato,clase){
@@ -50,4 +54,38 @@ function construirTd(dato,clase){
     td.classList.add(clase);
     td.textContent = dato;
     return td;
+}
+
+function validarPaciente(paciente){
+    var errores = []
+
+    if(paciente.nombre.length == 0){
+        errores.push("El nombre no puede estar vacío");
+    }
+    if(paciente.peso.length == 0){
+        errores.push("El peso no puede estar vacío");
+    }
+    if(paciente.altura.length == 0){
+        errores.push("La altura no puede estar vacía");
+    }
+    if(paciente.gordura.length == 0){
+        errores.push("El %gordura no puede estar vacío");
+    }
+    if(!validarPeso(paciente.peso)){
+        errores.push("El peso es incorrecto");
+    }
+    if(!validarAltura(paciente.altura)){
+        errores.push("La altura es incorrecta");
+    }
+    return errores; 
+}
+
+function exhibirMensajesErrores(errores){
+    var ul = document.querySelector("#mensajes-errores");
+    ul.innerHTML = ""
+    errores.forEach(function(error){
+        var li = document.createElement("li");
+        li.textContent = error;
+        ul.appendChild(li);
+    });
 }

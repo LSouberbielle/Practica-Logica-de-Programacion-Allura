f7bee6abf5279a4f4603d14b578aae5c2b66995a
Luciano Souberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-07-13 13:00:42 -0300

    Sort pacients

---
+++ JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/filtrar.js
@@ -0,0 +1,20 @@
+var campoFiltro = document.querySelector("#filtrar-tabla");
+
+campoFiltro.addEventListener("input", function(){
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+    for (var i = 0; i < pacientes.length; i++){
+        var paciente = pacientes[i];
+        var tdNombre = paciente.querySelector(".info-nombre");
+        var nombre = tdNombre.textContent;
+    }
+
+    if(nombre != this.value){
+        paciente.classList.add("invisible");
+    } else {
+        paciente.classList.remove("invisible");
+
+    }
+
+});

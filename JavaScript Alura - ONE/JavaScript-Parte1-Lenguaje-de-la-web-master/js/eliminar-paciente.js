a7cf1350328d32d07fb5fa6da6504b70a158b297
Luciano Souberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-07-13 11:57:26 -0300

    Remove pacientes CSS effect

--- JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/eliminar-paciente.js
+++ JavaScript Alura - ONE/JavaScript-Parte1-Lenguaje-de-la-web-master/js/eliminar-paciente.js
@@ -1,7 +1,9 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabla = document.querySelector("#tabla-pacientes");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dbclick",function(){
-        this.remove();
-    });
+tabla.addEventListener("click",function(event){
+    
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500); 
 });

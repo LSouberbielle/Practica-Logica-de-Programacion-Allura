43d582d87f1b2440701edb03f3b1954115d65ead
Luciano Souberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-07-15 13:37:08 -0300

    Prueba técnica
    Armado de formulario y tabla que recibe palabra a filtrar

--- JavaScript Alura - ONE/prueba tecnica/prueba-tecnica.js
+++ JavaScript Alura - ONE/prueba tecnica/prueba-tecnica.js
@@ -1,21 +1,50 @@
-//Inicializamos las palabras claves como un array, el programa podría incluir la opción de que el usuario cargue sus propias keywords
+//Inicializamos las palabras claves como un array, el programa podría incluir la opción de que el usuario cargue sus propias keywords.
 var keywords = ['a','aa','aaa']; 
 
-//Capturamos la palabra que deseamos filtrar a través de un input
+//Capturamos la palabra que deseamos filtrar a través de un input asociado a un botón.
 var botonFiltrar = document.querySelector("#filtrar-palabra");
 
 botonFiltrar.addEventListener("click",function(event){
     event.preventDefault();
 
-    var word = document.querySelector("#ingresar-palabra");
-    console.log(word);
+    var form = document.querySelector("#word-form")
+    var word = document.querySelector("#ingresar-palabra").value + "";
+    var wordTr = construirTr(word);
+
+    var error = validarPalabra(word);
+    
+    var tabla = document.querySelector("#tabla-palabras");
+    tabla.appendChild(wordTr);
+    form.reset();
    
 });
 
-function capturarPalabra(form){
+//Función que agrega la fila a la tabla, donde se construirá el Td con la palabra ingresada y el resultado de búsqueda de las palabras claves.
+function construirTr(word){
+    var wordTr = document.createElement("tr");
+    wordTr.appendChild(construirTd(word));
+    return wordTr;
+}
+
+//Función que asigna el Td al Tr.
+function construirTd(word){
+    var td = document.createElement("td");
+    td.textContent = word + " contiene las palabras claves " + "filtrado";
+    return td;
+}
+
+function validarPalabra(word){
+    var error = []
+
+    if(word.length == 0){
+        error.push("Introduzca una palabra para continuar");
+    }
+
+    return error;
 
 }
-    //var form = document.querySelector("#form-adicionar"); 
+
+//var form = document.querySelector("#form-adicionar"); 
 
 
 
